094aea6f3a67b07570ac21e55ea2397442af1638
evolumin <97439614+evolumin@users.noreply.github.com>
2023-03-08 11:59:51 +0800

    Update Chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -9,7 +9,7 @@ const COOKIE_NAME = 'nextjs-example-ai-chat-gpt3'
 export const initialMessages: ChatGPTMessage[] = [
   {
     role: 'assistant',
-    content: 'Hi! I am a friendly AI assistant. Ask me anything!',
+    content: '你好，我是七老师!',
   },
 ]
 
