b78f54ca6cac7d946ff2770099f9da1e888a4552
evolumin <97439614+evolumin@users.noreply.github.com>
2023-03-09 10:41:47 +0800

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -33,7 +33,7 @@ const handler = async (req: Request): Promise<Response> => {
   const payload: OpenAIStreamPayload = {
     model: 'gpt-3.5-turbo',
     messages: messages,
-    temperature: 1,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS
       ? parseInt(process.env.AI_MAX_TOKENS)
       : 100,
